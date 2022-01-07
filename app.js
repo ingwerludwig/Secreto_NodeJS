86f93d8f4672e6bc1b5eca81428031b2b7304cf6
Ingwer Ludwig <ingwerludwig@MacBook-Air-M1.local>
2022-01-07 11:24:39 +0700

    EJS Added

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const path = require('path')
 const userRoutes = require('./routes/userRoutes')
-const { engine } = require('express-handlebars');
+// const { engine } = require('express-handlebars');
+const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
 require('dotenv').config()
 const app = express()
@@ -11,8 +12,12 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
-app.engine('.hbs', engine({defaultLayout: 'main', extname: '.hbs'}));
-app.set('view engine', '.hbs')
+// app.engine('.hbs', engine({defaultLayout: 'main', extname: '.hbs'}));
+// app.set('view engine', '.hbs')
+app.use(expressLayouts)
+app.set('layout', './layouts/main')
+app.set('view engine', 'ejs')
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('', userRoutes)
